3548f2c09b7a37cf151fb3626f8169e9ba28d4d5
Metaphysiker <s.raess@me.com>
2023-07-01 17:03:38 +0200

    add npm utility container

--- vue/vue-app/src/components/InfoCard.vue
+++ vue/vue-app/src/components/InfoCard.vue
@@ -4,9 +4,30 @@
             <div class="card">
             <div class="card-body">
                 <h5 class="card-title">My Converter</h5>
+                <p>
+                    {{ message }}
+                </p>
                 <p class="card-text">Convert images to webp and videos to webm.</p>
             </div>
         </div>
     </div>
     </div>
 </template>
+
+<!--
+Say Hello World with Vue!
+-->
+
+<script setup>
+import { ref } from 'vue'
+import _ from 'lodash';
+// A "ref" is a reactive data source that stores a value.
+// Technically, we don't need to wrap the string with ref()
+// in order to display it, but we will see in the next
+// example why it is needed if we ever intend to change
+// the value.
+const result = _.capitalize("mccrazy");
+const message = ref('Hello World!')
+console.log("swag");
+console.log(result);
+</script>
