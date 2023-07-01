2098ac2d8a31100ae05429998b64eaf8adf22e10
Metaphysiker <s.raess@me.com>
2023-07-01 15:34:17 +0200

    trying to run on another computer

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-docker-compose run vue_develop
+docker-compose build
+docker-compuse up -d
+docker-compose run vue_develop // issues with path and ownership
+docker exec -it 6e2fbcd73478 sh // using this works to install
---
+++ vue/vue-app/src/components/InfoCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+const greeting = ref('Hello World!')
+</script>
+
+<template>
+    <div class="container">
+        <div class="text-center">
+            <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">My Converter</h5>
+                <p class="greeting">{{ greeting }}</p>
+
+                <p class="card-text">Convert images to webp and videos to webm.</p>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
