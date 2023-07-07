97f3782ebef1f4f62383701a7690d4fbc0bcc943
Metaphysiker <s.raess@me.com>
2023-07-07 08:04:39 +0200

    added methods to service

--- vue/vue-app/src/services/FileService.js
+++ vue/vue-app/src/services/FileService.js
@@ -17,6 +17,24 @@ class FileService {
         })
     }
 
+    removeFile(fileData){
+        let self = this;
+        return new Promise(function(final_resolve){
+            var formData = new FormData();
+            formData.append("fileName", fileData.fileName);
+
+            fetch(self.baseUrl + "remove_file", {
+                method: "POST",
+                body: formData
+              }).then((response) => response.json())
+              .then((json) => {
+                {
+                final_resolve(json);
+              }
+              })
+        })
+    }
+
     removeAllFiles(){
         let self = this;
         return new Promise(function(final_resolve){
